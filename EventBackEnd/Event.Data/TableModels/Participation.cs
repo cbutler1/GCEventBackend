4a183c840ceb7607e3b9b8990a40e2001aae546d
Brennan <brennan.davis@outlook.com>
2022-11-10 21:47:52 -0500

    Fixed Naming

--- EventBackEnd/Event.Data/TableModels/Participation.cs
+++ EventBackEnd/Event.Data/TableModels/Participation.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Event.Dataa.TableModels
+namespace Event.Data.TableModels
 {
     public class Participation
     {
--- EventBackEnd/Event.Data/TableModels/ThingToDo.cs
+++ EventBackEnd/Event.Data/TableModels/ThingToDo.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace Event.Dataa.TableModels
+namespace Event.Data.TableModels
 {
     public class ThingToDo
     {
